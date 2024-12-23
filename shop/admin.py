05beaa5e383709a741827c6dc94ed56943051f24
Giorgi <giorgitchikadze200@gmail.com>
2024-12-23 14:46:14 +0400

    Inital commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from shop.models import Item, Category, Tag
 
-# Register your models here.
+
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 1
+
+
+class TagInline(admin.StackedInline):
+    model = Item.tags.through
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ['name']
+    ordering = ['name']
+    inlines = [ItemInline]   
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'description')
+    search_fields = ['name', 'description']
+    ordering = ['-price']
+    inlines = [TagInline]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+
